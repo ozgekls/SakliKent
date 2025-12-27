e91ec1c13914d5e7260312c04b7daf1c2710ffbd
özge keleş <232201002@ogrenci.amasya.edu.tr>
2025-12-27 19:17:44 +0300

    deneme

--- lib/main.dart
+++ lib/main.dart
@@ -38,15 +38,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -83,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("deneme 1 2"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
